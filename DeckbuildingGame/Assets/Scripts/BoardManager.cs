a59bc2abc85ce379f1a616e8a88c09f6ec0908eb
Chris <Chris@localhost>
2019-04-15 13:11:43 -0400

    Started adding functionality for card effects and targeting.

---
+++ DeckbuildingGame/Assets/Scripts/BoardManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour
+{
+    public static BoardManager instance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void summonCreature(CreatureData creatureData, bool isFriendly)
+    {
+
+    }
+}
